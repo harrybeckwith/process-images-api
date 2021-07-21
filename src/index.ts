d5cba2d6a560230b4577ed10b390b190b9a6c94e
Harry Beckwith <harry.beckwith@bg-mac03.lan>
2021-07-21 13:40:11 +0100

    set up resize function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
-import fs from 'fs'
+import sharp from 'sharp'
 import path from 'path'
+import fs from 'fs'
 
 const app = express()
 const port = 3000
@@ -16,10 +17,64 @@ app.get('/api', function (req, res) {
 
     res.send(img)
     //  /images?filename=encenadaport&width=200&height=200
+    // fs.writeFile(path.join(__dirname,"assets/thumb",`${req.query.filename}.jpg`), "w+", () => {
+    //     console.log('a')
+    // })
+    const srcFilePath: string = path.join(
+        __dirname + '/assets/full/encenadaport.jpg'
+    );
 
-      fs.writeFileSync(path.join(__dirname,"assets/thumb",`${req.query.filename}.jpg`), "This is an example text","UTF8")
+    const dstFilePath: string = path.join(
+        __dirname + '/assets/thumb/encenadaport.jpg'
+    );
+
+    console.log(srcFilePath)
+
+    const resizeJPEG = async (
+        srcFilePath: string,
+        reqWidth: number,
+        reqHeight: number,
+        dstFilePath: string
+    ) => {
+        await sharp(srcFilePath)
+            .resize(reqWidth, reqHeight)
+            .toFormat('jpeg')
+            .toFile(dstFilePath);
+    };
+
+    try {
+        fs.accessSync(srcFilePath, fs.constants.F_OK);
+        console.log(`Requested file presents in full dir`);
+    } catch (err) {
+        res.send(`Error: Requested file is not present in full dir`);
+        return;
+    }
+
+       // Check if resized file is present or not
+       try {
+        fs.accessSync(dstFilePath, fs.constants.F_OK);
+        // File exists. Display the resized image
+        console.log(`Resized file present in the thumb dir`);
+
+        res.sendFile(dstFilePath);
+        return;
+    } catch (err) {
+        console.log(`No resized file found`);
+    }
+
+    const resize = async () => {
+        try {
+            await resizeJPEG(srcFilePath, 200, 300, dstFilePath);
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
+    resize()
 
 
+    
   })
 
   
