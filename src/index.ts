a4f7b561c6a5f3358b21fba1af7d27bc7cb3efb3
Harry Beckwith <harry.beckwith@bg-mac03.lan>
2021-07-21 11:05:45 +0100

    write file basic

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import fs from 'fs'
+import path from 'path'
 
 const app = express()
 const port = 3000
@@ -6,12 +8,21 @@ console.log(__dirname)
 app.use(express.static(__dirname));
 
 app.get('/api', function (req, res) {
-    res.send(`<img 
+    const img = `<img 
     src="/assets/full/${req.query.filename}.jpg" 
     width=${req.query.width}
     height=${req.query.height}
-    >`)
+    >`
+
+    res.send(img)
+    //  /images?filename=encenadaport&width=200&height=200
+
+      fs.writeFileSync(path.join(__dirname,"assets/thumb",`${req.query.filename}.jpg`), "This is an example text","UTF8")
+
+
   })
+
+  
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
