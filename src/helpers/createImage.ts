fdfe29d73f4606c33404d8393684b4e6741996ff
Harry Beckwith <harry.beckwith@bg-mac03.lan>
2021-07-22 08:48:43 +0100

    tests for createImage fnc

--- src/helpers/createImage.ts
+++ src/helpers/createImage.ts
@@ -1,11 +1,10 @@
-export default function (srcPath: string, width: unknown, height: unknown):string  {
+const createImage =  (srcPath: string, width: unknown, height: unknown) => {
 
 const imgWidth = width as number
 const imgHeight = height as number
 
-  return `<img 
-  src="${srcPath}" 
-  width=${Number(imgWidth)}
-  height=${Number(imgHeight)}
-  >`
+  return `<img src="${srcPath}" width=${Number(imgWidth)} height=${Number(imgHeight)}>`
 }
+
+
+export default createImage
--- src/tests/arraysSpec.ts
+++
@@ -1,6 +0,0 @@
-import  arrays from '../utilties/arrays'
-
-it('expect two arrays to concat', () => {
-  expect(arrays.concatArr([1,2,3], [4,5,6])).toEqual([1,2,3,4,5,6]);
-});
-
